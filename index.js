701da3208ec9914b97883758dee89dfa61763f64
RB\binoy.sinha <binoy.sinha@redbus.com>
2017-12-30 23:00:05 +0530

    changed message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send({message: 'Buy Buddy'});
 })
 const PORT = process.env.PORT || 5000;
 
